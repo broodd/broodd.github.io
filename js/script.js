2daa223d7c9c1983e09ef989742e77343c4d9210
BroodD <pidgurdkiy2000@gmail.com>
2017-06-26 23:27:42 +0300

    s

--- js/script.js
+++ js/script.js
@@ -23,15 +23,16 @@ $(document).on("click", "#sky", function() {
 $(window).scroll(function() {
 	var wScroll = $(this).scrollTop();
 
-	$("#roof").css({
-		"margin-top":wScroll/20 + "%"
-	});
+	if(screen.width > 890) 
+		$("#roof").css({
+			"margin-top":wScroll/20 + "%"
+		});
 
 	$(".up").css({
 		// "width":150 + wScroll/10 + "%",
-		// "left":-60-(wScroll/5) + "%",
-		"opacity":0 + ((wScroll-15)/1500),
-		// "transform":"rotate(" + (45 + (wScroll/10)) + "deg)"
+		"left":-60-(wScroll/5) + "%",
+		"opacity":0 + ((wScroll)/1500),
+		"transform":"rotate(" + (45 + (wScroll/10)) + "deg)"
 	});
 	console.log(wScroll);
 });
