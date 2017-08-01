ae6f9f2d6d41ee27a28388b144f8842323e467ff
BroodD <pidgurdkiy2000@gmail.com>
2017-08-01 16:11:18 +0300

    awdw

--- works/Baunty/js/script.js
+++ works/Baunty/js/script.js
@@ -4,26 +4,33 @@ $('.carousel').carousel({
   // wrap: true
 });
 
+// $('.item:last-child').addClass('active');
+// $('.ite:last-child').addClass('active');
+
+
+$("#sandwich, .menu_item").click(function() {
+    $("#sandwich").toggleClass("active_sandwich");
+});
 
 // $(document).ready(function(){
 //   $('.slick').slick({
 // 	  infinite: true,
-// 	  slidesToShow: 2,
-// 	  slidesToScroll: 1
+// 	  slidesToShow: 3,
+// 	  slidesToScroll: 3
 // 	});
 // });
 
 if (window.matchMedia('(max-width: 767px)').matches) {
-        $('.slick').slick({
-		  infinite: true,
-		  slidesToShow: 2,
-		  slidesToScroll: 1
-		});
-    } else {
-        $('.slick').slick({
-		  infinite: true,
-		  slidesToShow: 3,
-		  slidesToScroll: 1
-		});
+    $('.slick').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+} else {
+    $('.slick').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
 }
 
