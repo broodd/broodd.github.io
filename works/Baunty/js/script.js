c22d30fb6a5dd5d191966224dcd24f3748027106
BroodD <pidgurdkiy2000@gmail.com>
2017-08-01 15:59:22 +0300

    last die

--- works/Baunty/js/script.js
+++ works/Baunty/js/script.js
@@ -3,3 +3,27 @@ $('.carousel').carousel({
   // pause: 'hover',
   // wrap: true
 });
+
+
+// $(document).ready(function(){
+//   $('.slick').slick({
+// 	  infinite: true,
+// 	  slidesToShow: 2,
+// 	  slidesToScroll: 1
+// 	});
+// });
+
+if (window.matchMedia('(max-width: 767px)').matches) {
+        $('.slick').slick({
+		  infinite: true,
+		  slidesToShow: 2,
+		  slidesToScroll: 1
+		});
+    } else {
+        $('.slick').slick({
+		  infinite: true,
+		  slidesToShow: 3,
+		  slidesToScroll: 1
+		});
+}
+
