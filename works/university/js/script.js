a9fb5c9196a397b6b9aafc9a0cab26d2198b5009
BroodD <pidgurdkiy2000@gmail.com>
2017-09-06 11:18:58 +0300

    updete norm

--- works/university/js/script.js
+++ works/university/js/script.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
 		$('.navbar-toggle').toggleClass("act-btn");
 	});
 	$('.sidebar .caret').click(function() {
-		// var menu = $('.menu');
-		// var width1 = menu.width();
-		// menu.toggleClass('act_menu');
-		// $('.navbar-header').toggleClass('act_btn');
+		// var width1 = $('.client-info img').width();
+		// var width2 = $('.lient-info .inline-block').width();
+		// alert(width2);
 		$('.client-info img, .client-info .inline-block').slideToggle();
+		// $(this).css('margin-left', width1 + width2);
 	});
 });
