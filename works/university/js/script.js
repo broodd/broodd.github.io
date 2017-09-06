18e5e79d91747c6466003f76387e07807fffba62
BroodD <pidgurdkiy2000@gmail.com>
2017-09-06 10:55:48 +0300

    ok

--- works/university/js/script.js
+++ works/university/js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 	$('.navbar-toggle').click(function() {
-		var menu = $('.menu');
-		var width1 = menu.width();
-		menu.toggleClass('act_menu');
-		$('.navbar-header').toggleClass('act_btn');
+		// var menu = $('.menu');
+		// var width1 = menu.width();
+		// menu.toggleClass('act_menu');
+		// $('.navbar-header').toggleClass('act_btn');
+		$('.sidebar').slideToggle();
+		$('.navbar-toggle').toggleClass("act-btn");
+	});
+	$('.sidebar .caret').click(function() {
+		// var menu = $('.menu');
+		// var width1 = menu.width();
+		// menu.toggleClass('act_menu');
+		// $('.navbar-header').toggleClass('act_btn');
+		$('.client-info img, .client-info .inline-block').slideToggle();
 	});
 });
