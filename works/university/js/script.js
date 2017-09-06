f7133f14ad79f447115273ab8899d712f045de44
BroodD <pidgurdkiy2000@gmail.com>
2017-09-06 09:51:10 +0300

    delete all

--- works/university/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.navbar-toggle').click(function() {
-		var menu = $('.menu');
-		var width1 = menu.width();
-		menu.toggleClass('act_menu');
-		$('.navbar-header').toggleClass('act_btn');
-	});
-});
