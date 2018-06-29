263e58a0e68873f174a35de24816c0a46ff8a955
BroodD <pidgurdkiy2000@gmail.com>
2018-06-29 17:35:30 +0300

    some many

--- works/medal/js/script.js
+++ works/medal/js/script.js
@@ -5,71 +5,6 @@ $(document).ready(function(){
 	//     $(this).remove()
 	// });
 
-	$('#sandwich').click(function(event) {
-		$('.nav-main').slideToggle();
-	});
-
-	$('.icon-close').click(function(event) {
-		$(this).parents('.toggle').fadeOut();
-		$('.window').fadeOut();
-		// $('body').removeClass('overlay');
-	});
-
-	$('.hide-toggle').click(function(event) {
-		$(this).parents('.toggle').fadeOut();
-	});
-
-	$('[data-open]').click(function(event) {
-		$( $(this).attr('data-open') ).addClass('active-window').fadeIn();
-		// $('body').addClass('overlay');
-		$('.window').fadeIn();
-		return false;
-	});
-
-	$('.window').click(function(event) {
-		$('.active-window').fadeOut();
-		$(this).fadeOut();
-	});
-
-
-
-	$('.sidebar .block__title').click(function(event) {
-		var parent = $(this).parent();
-		
-		parent.toggleClass('active');
-		parent.children('.block__under').slideToggle();
-	});
-
-	$('.has-submenu a').click(function(event) {
-		$(this).parent('.has-submenu').children('.submenu').slideToggle();
-		return false;
-	});
-
-
-	// function changeView() {
-	// 	$('.nav-bottom').offset({left:0});
-	// }
-	// changeView();
-	// jQuery(window).resize( changeView());
-
-	// if( window.matchMedia("(max-width: 992px)").matches) {
-	$('.nav-main .menu > li.has-submenu').hover(function() {
-		// $('.nav-bottom').show();
-		$('.nav-bottom').addClass('active');
-	});
-
-	$('.nav-bottom').hover(function() {
-		$('.nav-bottom').addClass('active');
-	}, function() {
-		$('.nav-bottom').removeClass('active');
-	});
-
-
-
-	// $('.slick-wrap').slick({
-	 //  // slidesToShow: 1,
-	 //  // slidesToScroll: 1
-	// });
 
 	$('.slick__1').slick({
 		infinite: true,
@@ -104,5 +39,123 @@ $(document).ready(function(){
 			}
 		]
 	});
+
+
+	$('#sandwich').click(function(event) {
+		$('.nav-main').slideToggle();
+	});
+
+
+	$('.input__placeholder').on('click keypress focus', function(event) {
+		$(this).addClass('active');
+	});
+
+
+	$('.sidebar .block__title').click(function(event) {
+		var parent = $(this).parent();
+		
+		parent.toggleClass('active');
+		parent.children('.block__under').slideToggle();
+	});
+
+	$('.has-submenu > a').click(function(event) {
+		$(this).parent('.has-submenu').children('.submenu').slideToggle();
+		return false;
+	});
+
+
+	$('.icon-close').click(function(event) {
+		$(this).parents('.toggle').fadeOut();
+		$('.window').fadeOut();
+		// $('body').removeClass('overlay');
+	});
+
+	$('.hide-toggle').click(function(event) {
+		$(this).parents('.toggle').fadeOut();
+	});
+
+	$('[data-open]').click(function(event) {
+		$( $(this).attr('data-open') ).addClass('active-window').fadeIn();
+		// $('body').addClass('overlay');
+		$('.window').fadeIn();
+		return false;
+	});
+
+	$('.window').click(function(event) {
+		$('.active-window').fadeOut();
+		$(this).fadeOut();
+	});
+
+
+	function changeView() {
+		if( window.matchMedia("(min-width: 768px)").matches) {
+			$('.nav-bottom .menu-wrap [data-hover]').hover(function() {
+				$('.nav-bottom .menu-wrap .sub-menu').removeClass('active');
+				$( $(this).attr('data-hover') ).addClass('active');
+			}, function() {
+				$('.nav-bottom .menu-wrap .submenu').removeClass('active');
+			});
+
+			$('.nav-bottom .menu-wrap .submenu').hover(function() {
+				$(this).addClass('active');
+			}, function() {
+				$(this).removeClass('active');
+			});
+
+
+
+			$('.nav-main .menu > li.has-submenu').hover(function() {
+				$('.nav-bottom').addClass('active');
+			});
+
+			$('.nav-bottom').hover(function() {
+				$(this).addClass('active');
+			}, function() {
+				$(this).removeClass('active');
+			});
+
+
+			$('.nav .has-submenu > a').click(function(event) {
+				window.location = $(this).attr('href');
+			});
+		} else return false;
+	}
+	changeView();
+	$(window).resize(function() {
+		console.log('re'),
+		changeView();
+	});
+
+
+	$('.count__minus').click(function(event) {
+		var input = $(this).next();
+		if( parseInt(input.val()) > 1 )
+			input.val( parseInt(input.val()) - 1);
+		else input.val(1);
+	});
+
+	$('.count__plus').click(function(event) {
+		var input = $(this).prev();
+		if( parseInt(input.val()) > 0)
+			input.val( parseInt(input.val()) + 1);
+		else input.val(1);
+	});
+
+	$('.count__close').click(function(event) {
+		$(this).parents('.count__row').remove();
+	});
+
+	// $('.count__input').change(function(event) {
+	// 	$(this).parents('.count__row').children('[data-option="count-sum"]').
+	// });
+
+	// function changeView() {
+	// 	$('.nav-bottom').offset({left:0});
+	// }
+	// changeView();
+	// jQuery(window).resize( changeView());
+	// if( window.matchMedia("(max-width: 992px)").matches) {
+
+
 
 });
