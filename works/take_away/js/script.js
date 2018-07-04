a9c69ee5d7fe0df422af5b567b68e5376c003ff2
BroodD <pidgurdkiy2000@gmail.com>
2018-07-04 21:19:10 +0300

    my-version

--- works/take_away/js/script.js
+++ works/take_away/js/script.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
 
 	// preloader
-    $(".preloader-background").delay(200).fadeOut(500, function() {
-        $(this).remove()
-    });
+	// $(".preloader-background").delay(200).fadeOut(500, function() {
+	// 	$(this).remove()
+	// });
 
+	$('.slick-vertical').slick({
+		slidesToShow: 5,
+		asNavFor: '.slick-vertical_for',
+		dots: false,
+		arrows: true,
+		centerMode: false,
+		focusOnSelect: true,
+		infinite: true,
+		vertical: true,
+		swipe: false
+	});
+
+	$('.slick-vertical_for').slick({
+		slidesToShow: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slick-vertical',
+		// adaptiveHeight: true,
+		swipe: false
+	});
 });
