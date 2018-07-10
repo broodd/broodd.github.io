4ae4ee3e0d13871b8c8fa43dcfb52a8f656df21d
BroodD <pidgurdkiy2000@gmail.com>
2018-07-10 22:59:24 +0300

    asdda

--- works/take_away/js/script.js
+++ works/take_away/js/script.js
@@ -125,4 +125,46 @@ $(document).ready(function(){
 		$(this).addClass('active');
 	});
 	$('.tab .tablink:first-child').click();
+
+
+
+  $('.wrapper-section .title-arrow').click(function(){
+    $(this).toggleClass('open');
+    // $(this).next('div.wrap-selector__list').slideToggle();
+    $(this).next('.wrap-selector__list').toggleClass('open');;
+  });
+
+
+	$( function() {
+    $( "#wrapper-section__slider" ).slider({
+      range: true,
+      min: 0,
+      max: 100000,
+      values: [ 1755, 84556 ],
+      slide: function( event, ui ) {
+        $( "#number-1" ).val( ui.values[ 0 ] );
+        $( "#number-2" ).val( ui.values[ 1 ] );
+      }
+    });
+    $( "#number-1" ).val( $( "#wrapper-section__slider" ).slider( "values", 0 ) );
+    $( "#number-2" ).val( $( "#wrapper-section__slider" ).slider( "values", 1 ) );
+  } );
+  $("input#number-1").change(function(){
+    var value1=$("input#number-1").val();
+    var value2=$("input#number-2").val();
+    if(parseInt(value1) > parseInt(value2)){
+      value1 = value2;
+      $("input#number-1").val(value1);
+    }
+    $("#wrapper-section__slider").slider("values",0,value1);
+  });
+  $("input#number-2").change(function(){
+    var value1=$("input#number-1").val();
+    var value2=$("input#number-2").val();
+    if(parseInt(value1) > parseInt(value2)){
+      value2 = value1;
+      $("input#number-2").val(value2);
+    }
+    $("#wrapper-section__slider").slider("values",1,value2);
+  });
 });
