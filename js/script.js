6142e4461d3fa1c7927b1844ce1e60c0857ddfbc
BroodD <pidgurdkiy2000@gmail.com>
2018-09-14 13:56:16 +0300

    preloader

--- js/script.js
+++ js/script.js
@@ -1,13 +1,48 @@
+function loaderSpinner() {
+  $(window).load(function() {
+    var loader = $('.loader');
+    // var wHeight = $(window).height();
+    // var wWidth = $(window).width();
+    var i = 0;
+    /*Center loader on half screen */
+    // loader.css({
+    //   top: wHeight / 2 - 2.5,
+    //   left: wWidth / 2 - 200
+    // });
+    
+    do{
+      loader.animate({
+        width: i
+      },10)
+      i+=3;
+    } while(i <= 400)
+    if(i === 402){
+      loader.animate({
+        left: 0,
+        width: '100%'
+      })
+      loader.animate({
+        top: '0',
+        height: '100vh'
+      })
+    }
+    
+    /* This line hide loader and show content */
+    setTimeout(function(){
+      $('.loader-wrap').fadeIn("slow");
+      (loader).fadeOut("fast");
+      /*Set time in milisec */
+    },3500);
+  });
+
+}
+
+loaderSpinner();
+
+
+
 $(document).ready(function(){
 	// // preloader
-	// $(".preloader-background").delay(200).fadeOut(500, function() {
-	// 	$(this).remove()
-	// });
-
-	// $('a').not('a[href="#"]').click(function(event) {
-	// 	alert('clikc');
-	// });
-
 
 	$('.js-tilt').tilt({
 		maxTilt: 8,
