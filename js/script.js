5bfe61ec80e5924ec96e3cec4bb9292b3fac9ba4
BroodD <pidgurdkiy2000@gmail.com>
2018-10-07 22:33:21 +0300

    no important

--- js/script.js
+++ js/script.js
@@ -1,46 +1,3 @@
-function loaderSpinner() {
-  $(window).load(function() {
-    var loader = $('.loader');
-    // var wHeight = $(window).height();
-    // var wWidth = $(window).width();
-    var i = 0;
-    /*Center loader on half screen */
-    // loader.css({
-    //   top: wHeight / 2 - 2.5,
-    //   left: wWidth / 2 - 200
-    // });
-    
-    do{
-      loader.animate({
-        width: i
-      },10)
-      i+=3;
-    } while(i <= 400)
-    if(i === 402){
-      loader.animate({
-        left: 0,
-        width: '100%'
-      })
-      loader.animate({
-        top: '0',
-        height: '100vh'
-      })
-    }
-    
-    /* This line hide loader and show content */
-    setTimeout(function(){
-      $('.loader-wrap').fadeIn("slow");
-      (loader).fadeOut("fast");
-      /*Set time in milisec */
-    },3500);
-  });
-
-}
-
-loaderSpinner();
-
-
-
 $(document).ready(function(){
 	// // preloader
 
@@ -63,12 +20,7 @@ $(document).ready(function(){
 	});
 
 
-	// $('.js-hover-parallax').mousemove(function(event) {
-	// 	th   = $(this),
-	// 	item = $('.js-hover-parallax .item');
-	// });
-
-	$(function() {
+	(function() {
 		var form = $('#form'),
 				formMessages = $('#form-messages');
 
@@ -93,10 +45,7 @@ $(document).ready(function(){
 					formMessages.text('Oops! An error occured and your message could not be sent.');
 				}
 			}).done(function(data) {
-				console.log(typeOf(data));
-				console.log(data);
 				data = JSON.parse(data);
-				console.log(data);
 				if ( !data.success) {
 					formMessages.addClass('error');
 
@@ -111,13 +60,12 @@ $(document).ready(function(){
 					formMessages.text(data.message);
 				}
 			});
-		});
+		}());
 	});
 
 });
 
 
-// random items from array
 function randomFromArray(arg) {
 	return arg[ Math.floor(Math.random() * arg.length) ];
 }
--- php.php
+++
@@ -1,3 +0,0 @@
-RewriteEngine on 
-
-RewriteRule ^(home|contanct)$ $1.php [NC,L]
